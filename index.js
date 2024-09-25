cc15171013360c7db57100d918a760c4fe31fdf0
Ravi Jetani <ravi.jetani@codento.com>
2024-09-25 18:03:01 +0530

    initial commit for google cloud function

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // require('dotenv').config();
+const functions = require('@google-cloud/functions-framework');
 const whatsappController = require('./whatsappController');
-async function main(req,res) {
-if (req.method === 'GET') {
+
+functions.http('facebookWebhookProxyV2', async (req, res) => {
+  if (req.method === 'GET') {
     whatsappController.verifyWebhook(req, res);
   } else if (req.method === 'POST') {
     await whatsappController.handleWebhook(req, res);
   } else {
     res.status(405).send('Method Not Allowed');
   }
-
-}
+});
